e3af236520b1208102218c6f90898fe77f91a0cf
hmbarrazav <humbarraza@upt.pe>
2022-03-31 10:07:37 -0500

    Implementación de la clase Universidad

--- Rector.cpp
+++ Rector.cpp
@@ -17,8 +17,30 @@ void Rector :: mostrarRector(){
     cout<<"Dr. "<<nombres;
 }
 
+class Universidad{
+    private:
+        string denominacion;
+        string ciudad;
+    public:
+        Universidad(string,string);
+        void mostrarDatos();
+};
+
+Universidad :: Universidad(string d, string c){
+    denominacion = d;
+    ciudad = c;
+}
+
+void Universidad :: mostrarDatos(){
+    cout<<"La Universidad "<<denominacion<<" esta ubicada ";
+    cout<<"en la ciudad de "<<ciudad;
+}
+
 int main(){
     Rector r1("Javier Lozano");
     r1.mostrarRector();
+    cout<<endl;
+    Universidad u1("Jorge Basadre Grohmann","Tacna");
+    u1.mostrarDatos();
     return 0;
 }
