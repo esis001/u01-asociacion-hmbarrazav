3503568ba2a406df13ee150a0691ec50b106d064
hmbarrazav <humbarraza@upt.pe>
2022-03-31 10:25:55 -0500

    Implementación del atributo de agregación en la clase Universidad

--- Rector.cpp
+++ Rector.cpp
@@ -21,19 +21,32 @@ class Universidad{
     private:
         string denominacion;
         string ciudad;
+        Rector *r;
     public:
         Universidad(string,string);
         void mostrarDatos();
+        void asignarRector(Rector*);
 };
 
 Universidad :: Universidad(string d, string c){
     denominacion = d;
     ciudad = c;
+    r = NULL;
 }
 
 void Universidad :: mostrarDatos(){
     cout<<"La Universidad "<<denominacion<<" esta ubicada ";
     cout<<"en la ciudad de "<<ciudad;
+    if (r == NULL)
+        cout<<" aun no tiene rector asignado";
+    else{
+        cout<<" y tiene como rector al "; 
+        r->mostrarRector();
+    }
+}
+
+void Universidad :: asignarRector(Rector *pR){
+    r = pR;
 }
 
 int main(){
@@ -41,6 +54,9 @@ int main(){
     r1.mostrarRector();
     cout<<endl;
     Universidad u1("Jorge Basadre Grohmann","Tacna");
+    u1.mostrarDatos();
+    u1.asignarRector(&r1);
+    cout<<endl;
     u1.mostrarDatos();
     return 0;
 }
