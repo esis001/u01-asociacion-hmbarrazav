e7189572f0ce58bbaabcea0565ddbe7cc3da0c37
hmbarrazav <humbarraza@upt.pe>
2022-04-01 08:01:46 -0500

    Implementación de la clase Duenio

--- Casas.cpp
+++ Casas.cpp
@@ -20,8 +20,27 @@ void Casa :: mostrarCasa(){
     cout<<cantHab<<" habitaciones."<<endl;
 }
 
+class Duenio{
+    private:
+        string nombre;
+    public:
+        Duenio(string);
+        void mostrarDuenio();
+};
+
+Duenio :: Duenio(string n){
+    nombre = n;
+}
+
+void Duenio :: mostrarDuenio(){
+    cout<<"El(la) contribuyente "<<nombre<<" esta ";
+    cout<<"registrado(a) en el sistema."<<endl;
+}
+
 int main(){
     Casa c1("Av. San Martin 888", 4);
     c1.mostrarCasa();
+    Duenio d1("Jorge Perez");
+    d1.mostrarDuenio();
     return 0;
 }
