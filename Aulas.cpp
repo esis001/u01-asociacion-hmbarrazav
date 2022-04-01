b5d83ae185cccaa04edb1f588901303b80941dc8
hmbarrazav <humbarraza@upt.pe>
2022-04-01 09:04:19 -0500

    Implementación de los atributos de composición en la clase Colegio

--- Aulas.cpp
+++ Aulas.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<string>
 using namespace std;
 
 class Aula{
@@ -23,23 +24,54 @@ void Aula :: mostrarAula(){
 class Colegio{
     private:
         string nombre;
+        Aula* secciones[10];
+        int nAulas;
     public:  
         Colegio(string);
         void mostrarColegio();
+        void leerAulas(int);
 };
 
 Colegio :: Colegio(string n){
     nombre = n;
+    nAulas = 0;
 }
 
 void Colegio :: mostrarColegio(){
-    cout<<"El colegio "<<nombre<<endl;
+    cout<<"El colegio "<<nombre<<", ";
+    if(nAulas == 0){
+        cout<<"no tiene aulas. "<<endl;
+    }
+    else{
+        cout<<"tiene "<<nAulas<<" aulas:"<<endl;
+        for(int i=0; i<nAulas; i++){
+            cout<<"\t-";
+            secciones[i]->mostrarAula();
+        }
+    }
+}
+
+void Colegio :: leerAulas(int n){
+    string nom;
+    int nA;
+    for(int i=0; i<n; i++){
+        cout<<"Ingrese nombre del Aula: ";
+        fflush(stdin); getline(cin,nom);
+        cout<<"Ingrese aforo: "; cin>> nA;
+        secciones[nAulas] = new Aula(nom,nA);
+        nAulas++;
+    }
 }
 
 int main(){
-    Aula a1("Primer grado",20);
-    a1.mostrarAula();
+    //Aula a1("Primer grado",20);
+    //a1.mostrarAula();
+    int cA;
     Colegio c1("Santa Teresita");
+    c1.mostrarColegio();
+    cout<<"----------------"<<endl;
+    cout<<"Ingrese cantidad de aulas: "; cin>>cA;
+    c1.leerAulas(cA);
     c1.mostrarColegio();
     return 0;
 }
