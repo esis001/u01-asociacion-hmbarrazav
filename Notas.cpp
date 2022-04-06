95821240b8242f68e6bacacdb8c567ca0c2bff1d
hmbarrazav <hmbarrazav@gmail.com>
2022-04-06 11:32:04 -0500

    Implementación de los getters de Estudiante

--- Notas.cpp
+++ Notas.cpp
@@ -26,23 +26,48 @@ class Estudiante : public Persona{
         string codigo;
         int notas[10];
         int nNotas;
-        Docente *docente;
+        Docente *docente; //atributo de asociación
     public:
         Estudiante(string,string,string);
-        void ingresarNota(int);
         void mostrarEstudiante();
         void asignarDocente(Docente *);
+        void ingresarNota(int);
+        string getCodigo(); //getters
+        string getNombres();
+        int getNNotas();
+        int* getNotas();
 }; 
+
+string Estudiante :: getCodigo(){
+    return codigo;
+}
+
+string Estudiante :: getNombres(){
+    return nombres;
+}
+
+int Estudiante :: getNNotas(){
+    return nNotas;
+}
+
+int* Estudiante :: getNotas(){
+    return notas;
+}
+
+void Estudiante :: ingresarNota(int n){
+    if(nNotas < 10){
+        notas[nNotas] = n;
+        nNotas++;
+    }
+    else{
+        cout<<"No se pudo insertar la nota"<<endl;
+    }
+}
 
 Estudiante :: Estudiante(string n, string d, string c)
     : Persona(n,d){
     codigo = c;
     nNotas = 0;
-}
-
-void Estudiante :: ingresarNota(int n){
-    notas[nNotas] = n;
-    nNotas++;
 }
 
 void Estudiante :: mostrarEstudiante(){
@@ -72,13 +97,18 @@ class Docente : public Persona{
     private:
         string especialidad;
         float sueldo;
-        Estudiante* listaEstudiantes[30];
-        int nEstudiantes;
+        Estudiante* listaEstudiantes[30]; //atributo de asociación
+        int nEstudiantes; //atributo de asociación
     public:
         Docente(string,string,string,float);
         void mostrarDocente();
         void asignarEstudiante(Estudiante*);
+        void ingresarNotaEst(int, int);
 };
+
+void Docente :: ingresarNotaEst(int nota, int nE){
+    listaEstudiantes[nE] -> ingresarNota(nota);
+}
 
 Docente :: Docente(string d, string n, string e, float s)
     : Persona(d,n){
@@ -109,7 +139,9 @@ void Docente :: mostrarDocente(){
 void Docente :: asignarEstudiante(Estudiante *pE){
     if(nEstudiantes <= 30){
         listaEstudiantes[nEstudiantes] = pE;
-        pE->asignarDocente(this);
+        pE->asignarDocente(this); //asigna el docente actual
+                                  //al estudiante del parámetro
+                                  //de entrada
         nEstudiantes++;
     }
     else
@@ -117,9 +149,9 @@ void Docente :: asignarEstudiante(Estudiante *pE){
 }
 
 int main(){
-    Persona per1("12345678", "Fulatino de Tal");
-    per1.mostrarDatos();
-    cout<<endl;
+    //Persona per1("12345678", "Fulatino de Tal");
+    //per1.mostrarDatos();
+    //cout<<endl;
     Estudiante est1("10203040","Jorge Perez","2021-123456");
     est1.mostrarEstudiante();
     est1.ingresarNota(16);
@@ -128,12 +160,20 @@ int main(){
     cout<<"-------------------"<<endl;
     est1.mostrarEstudiante();
     cout<<"-------------------"<<endl;
-    Docente doc("00102030","Pablo Mamani","Ing. Quimico",5000.00);
+    /*Docente doc("00102030","Pablo Mamani","Ing. Quimico",5000.00);
     doc.mostrarDocente();
     Estudiante est2("10203041","Andrez Gomez","2021-123457");
     doc.asignarEstudiante(&est1);
     doc.asignarEstudiante(&est2);
     cout<<"-------------------"<<endl;
-    doc.mostrarDocente();
+    doc.mostrarDocente();*/
+    cout<<"Codigo: "<<est1.getCodigo()<<endl;
+    cout<<"Nombres: "<<est1.getNombres()<<endl;
+    cout<<"Cantidad de notas: "<<est1.getNNotas()<<endl;
+    //cout<<"Notas: "<<*est1.getNotas()<<endl;
+    //cout<<"Notas: "<<*(est1.getNotas()+1)<<endl;
+    for(int i=0; i<est1.getNNotas();i++){
+        cout<<"Nota #"<<i+1<<": "<<*(est1.getNotas()+i)<<endl;
+    }
     return 0;
 }
